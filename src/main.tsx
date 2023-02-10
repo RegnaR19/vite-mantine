ff35f49a61b7815fd4b0cacb7e15c32f54a5b205
RegnaR <nar200@yandex.ru>
2023-02-10 15:48:38 +0500

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import state, { subscribe } from "./redux/state";
-import { addPost, updateNewPostText } from "./redux/state";
+import Store from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!)
 let rerenderEntireTree = (state: any) => {
-   root.render(<App state={state} addPost={addPost}
-      updateNewPostText={updateNewPostText} />);
+   root.render(<App state={state}
+      addPost={Store.addPost.bind(Store)}
+      updateNewPostText={Store.updateNewPostText.bind(Store)} />);
 }
 
+rerenderEntireTree(Store.getState());
 
-rerenderEntireTree(state);
-
-subscribe(rerenderEntireTree)
+Store.subscribe(rerenderEntireTree)
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -2,53 +2,54 @@ import naruto from "../assets/naruto.jpg";
 import saske from "../assets/saske2.jpg";
 import rem from "../assets/rem.webp";
 
-let rerenderEntireTree = () => {
-
-}
-
-let state = {
-   dialogsPage: {
-      dialogs: [
-         { id: 1, name: "Валера" },
-         { id: 2, name: "Димон" },
-         { id: 3, name: "Руслан" }
-      ],
-      messages: [
-         { id: 1, message: "Hi", path: "/profile" },
-         { id: 2, message: "How are you? Im in Almenevo today", path: "/profile" },
-         { id: 3, message: "Hello", path: "/profile" },
-      ]
+let Store = {
+   _state: {
+      dialogsPage: {
+         dialogs: [
+            { id: 1, name: "Валера" },
+            { id: 2, name: "Димон" },
+            { id: 3, name: "Руслан" }
+         ],
+         messages: [
+            { id: 1, message: "Hi", path: "/profile" },
+            { id: 2, message: "How are you? Im in Almenevo today", path: "/profile" },
+            { id: 3, message: "Hello", path: "/profile" },
+         ]
+      },
+      postPage: {
+         posts: [
+            { id: 1, post: "Даттебайо", message: "Я тут новенькая", likescount: 300, img: rem },
+            { id: 2, post: "Chidori!", message: "НАРУТО!!!11!", likescount: 1000, img: saske },
+            { id: 3, post: "Rasengan!", message: "САСКЕ!!!11!", likescount: 5000, img: naruto },
+         ],
+         newPostText: "dattebayo"
+      }
    },
-   postPage: {
-      posts: [
-         { id: 1, post: "Даттебайо", message: "Я тут новенькая", likescount: 300, img: rem },
-         { id: 2, post: "Chidori!", message: "НАРУТО!!!11!", likescount: 1000, img: saske },
-         { id: 3, post: "Rasengan!", message: "САСКЕ!!!11!", likescount: 5000, img: naruto },
-      ],
-      newPostText: "dattebayo"
+   getState() {
+      return this._state
+   },
+   _callSubscriber() {
+      return this._state
+   },
+   addPost() {
+      let newPost = {
+         id: 4,
+         post: "Новый пост",
+         message: this._state.postPage.newPostText,
+         likescount: 0,
+         img: rem
+      }
+      this._state.postPage.posts.push(newPost)
+      this._state.postPage.newPostText = ''
+      this._callSubscriber(this._state)
+   },
+   updateNewPostText(newText: any) {
+      this._state.postPage.newPostText = newText
+      this._callSubscriber(this._state)
+   },
+   subscribe(observer: any) {
+      this._callSubscriber = observer;
    }
 }
 
-export const addPost: any = () => {
-   let newPost = {
-      id: 4,
-      post: "Новый пост",
-      message: state.postPage.newPostText,
-      likescount: 0,
-      img: rem
-   }
-   state.postPage.posts.push(newPost)
-   state.postPage.newPostText = ''
-   rerenderEntireTree()
-}
-
-export const updateNewPostText: any = (newText: any) => {
-   state.postPage.newPostText = newText
-   rerenderEntireTree()
-}
-
-export const subscribe = (observer: any) => {
-   rerenderEntireTree = observer;
-}
-
-export default state
+export default Store
