612acbd51446be416ddf4a226ca1039d0fb8778c
RegnaR <nar200@yandex.ru>
2023-02-27 16:19:22 +0500

    up

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,46 +1,37 @@
-import { Button } from "@mantine/core";
-import { IconUserPlus } from "@tabler/icons";
-import { Component, useState } from "react";
 import Indent10 from "../Forms/Indent";
 import UsersPage from "./UsersPage";
 import axios from 'axios'
+import React from "react";
 
 type Props = {
    users: any,
    setUsers: any
 }
 
-class Users extends Component<Props> {
+class Users extends React.Component<Props> {
 
-   getUsers = () => {
-      if (this.props.users.length > 0) {
-         axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response =>
-               this.props.setUsers(response.data.items)
-            )
-      }
+   constructor(props: any) {
+      super(props)
+
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+         .then(response =>
+            this.props.setUsers(response.data.items)
+         )
    }
 
    usersElements = () => {
-      this.props.users.map((e: any) =>
+      return this.props.users.map((e: any) =>
          <UsersPage id2={e.id} name={e.name}
             status={e.status} city={e.city} img={e.img} />)
    }
 
-   log = () => { console.log(this.getUsers) }
-
    render() {
       return (
-         <>
+         <div>
+            <p>Ку</p>
             {this.usersElements}
             <Indent10 />
-            <Button variant="outline"
-               leftIcon={<IconUserPlus />}
-               gradient={{ from: 'indigo', to: 'cyan' }}
-               onClick={this.getUsers}>
-               Получить пользователей
-            </Button>
-         </>
+         </div>
       )
    }
 }
